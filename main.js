06415b63d7934998bd76bc481ce3b0ae81f58fe7
davidnherran <davidnherran@gmail.com>
2022-02-09 23:39:22 -0500

    :building_construction: feature(add: share code snippets by url with base64)

--- main.js
+++ main.js
@@ -1,40 +1,61 @@
-import Split from "split-grid";
-import "./style.css";
+import Split from 'split-grid'
+import './style.css'
 
-const $ = (selector) => document.querySelector(selector);
+const $ = (selector) => document.querySelector(selector)
 
 Split({
   columnGutters: [
     {
       track: 1,
-      element: $(".gutter-col-1"),
-    },
+      element: $('.gutter-col-1')
+    }
   ],
   rowGutters: [
     {
       track: 1,
-      element: $(".gutter-row-1"),
-    },
-  ],
-});
+      element: $('.gutter-row-1')
+    }
+  ]
+})
 
-const $js = $("#js");
-const $css = $("#css");
-const $html = $("#html");
+const $js = $('#js')
+const $css = $('#css')
+const $html = $('#html')
 
-$js.addEventListener("input", update);
-$css.addEventListener("input", update);
-$html.addEventListener("input", update);
+$js.addEventListener('input', update)
+$css.addEventListener('input', update)
+$html.addEventListener('input', update)
 
-function update() {
-  const html = createHTML();
-  $("iframe").setAttribute("srcdoc", html);
+function init () {
+  const { pathname } = window.location
+
+  const [rawHtml, rawCss, rawJs] = pathname.slice(1).split('%7C')
+
+  const html = window.atob(rawHtml)
+  const css = window.atob(rawCss)
+  const js = window.atob(rawJs)
+
+  $html.value = html
+  $css.value = css
+  $js.value = js
+
+  const htmlForPreview = createHTML({ html, js, css })
+  $('iframe').setAttribute('srcdoc', htmlForPreview)
 }
 
-const createHTML = () => {
-  const html = $html.value;
-  const css = $css.value;
-  const js = $js.value;
+function update () {
+  const html = $html.value
+  const css = $css.value
+  const js = $js.value
+
+  const hashedCode = `${window.btoa(html)}|${window.btoa(css)}|${window.btoa(
+    js
+  )}`
+
+  window.history.replaceState(null, null, `/${hashedCode}`)
+}
+
+const createHTML = ({ html, js, css }) => {
   return `
     <!DOCTYPE html>
     <html lang="es">
@@ -50,5 +71,6 @@ const createHTML = () => {
       ${html}
     </body>
     </html>
-  `;
-};
+  `
+}
+init()
