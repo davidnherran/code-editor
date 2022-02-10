bbbec2a8ea78009a4f9770d82297bb2cba947610
davidnherran <davidnherran@gmail.com>
2022-02-09 21:10:25 -0500

    :egg: chore(create: structure and basic functionality)

--- main.js
+++ main.js
@@ -1,6 +1,38 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const $ = selector => document.querySelector(selector)
+
+const $js = $('#js')
+const $css = $('#css')
+const $html = $('#html')
+
+$js.addEventListener('input', update)
+$css.addEventListener('input', update)
+$html.addEventListener('input', update)
+
+function update() {
+  const html = createHTML()
+  $('iframe').setAttribute('srcdoc', html)
+}
+
+const createHTML = () => {
+  const html = $html.value
+  const css = $css.value
+  const js = $js.value
+  return `
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
+      <style>
+        ${css}
+      </style>
+    </head>
+    <script>
+      ${js}
+    </script>
+    <body>
+      ${html}
+    </body>
+    </html>
+  `
+}
