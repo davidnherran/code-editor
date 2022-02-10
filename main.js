c216f15658d4d552114938ddae57d20407950445
davidnherran <davidnherran@gmail.com>
2022-02-10 01:53:12 -0500

    :recycle: refactoring(change: change editor in textarea for monaco by vscode)

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 import Split from 'split-grid'
 import { encode, decode } from 'js-base64'
+import * as monaco from 'monaco-editor'
+import HtmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
+import CssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
+import JsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
 import './style.css'
+
+window.MonacoEnviorement = {
+  getWorker (_, label) {
+    if (label === 'html') return new HtmlWorker()
+    if (label === 'css') return new CssWorker()
+    if (label === 'typescript' || label === 'javascript') return new JsWorker()
+  }
+}
 
 const $ = (selector) => document.querySelector(selector)
 
@@ -23,31 +35,44 @@ const $js = $('#js')
 const $css = $('#css')
 const $html = $('#html')
 
-$js.addEventListener('input', update)
-$css.addEventListener('input', update)
-$html.addEventListener('input', update)
+const { pathname } = window.location
 
-function init () {
-  const { pathname } = window.location
+const [rawHtml, rawCss, rawJs] = pathname.slice(1).split('%7C')
 
-  const [rawHtml, rawCss, rawJs] = pathname.slice(1).split('%7C')
+const html = decode(rawHtml)
+const css = decode(rawCss)
+const js = decode(rawJs)
 
-  const html = decode(rawHtml)
-  const css = decode(rawCss)
-  const js = decode(rawJs)
+const htmlEditor = monaco.editor.create($html, {
+  language: 'html',
+  theme: 'vs-dark',
+  value: html,
+  fontSize: 17
+})
+const cssEditor = monaco.editor.create($css, {
+  language: 'css',
+  theme: 'vs-dark',
+  value: css,
+  fontSize: 17
+})
+const jsEditor = monaco.editor.create($js, {
+  language: 'javascript',
+  theme: 'vs-dark',
+  value: js,
+  fontSize: 17
+})
 
-  $html.value = html
-  $css.value = css
-  $js.value = js
+htmlEditor.onDidChangeModelContent(update)
+cssEditor.onDidChangeModelContent(update)
+jsEditor.onDidChangeModelContent(update)
 
-  const htmlForPreview = createHTML({ html, js, css })
-  $('iframe').setAttribute('srcdoc', htmlForPreview)
-}
+const htmlForPreview = createHTML({ html, js, css })
+$('iframe').setAttribute('srcdoc', htmlForPreview)
 
 function update () {
-  const html = $html.value
-  const css = $css.value
-  const js = $js.value
+  const html = htmlEditor.getValue()
+  const css = cssEditor.getValue()
+  const js = jsEditor.getValue()
 
   const hashedCode = `${encode(html)}|${encode(css)}|${encode(js)}`
 
@@ -57,7 +82,7 @@ function update () {
   $('iframe').setAttribute('srcdoc', htmlForPreview)
 }
 
-const createHTML = ({ html, js, css }) => {
+function createHTML ({ html, js, css }) {
   return `
     <!DOCTYPE html>
     <html lang="es">
@@ -75,4 +100,3 @@ const createHTML = ({ html, js, css }) => {
     </html>
   `
 }
-init()
